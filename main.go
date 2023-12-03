574678e42bc3d606536be18dd8eb6672727f4a83
addykan <addykan@gmail.com>
2023-12-03 00:47:53 -0500

    Update benchmarking infra and add buggy latch crabbing for insert function

--- main.go
+++ main.go
@@ -3,48 +3,53 @@ package main
 import (
 	"fmt"
 	"main/benchmark"
+	"main/crab"
 	"main/global_lock_tree"
 	"main/seq_tree"
+	"main/tree_api"
+	"time"
 )
 
+func runSpeedup(trees []tree_api.BPTree, keyCount int, threadCount int, benchmarkFunc func(tree_api.BPTree, int, int) (time.Duration, float64)) {
+	duration, _ := benchmarkFunc(trees[0], keyCount, 1)
+	for i := 1; i < len(trees); i++ {
+		threadCount := 1 << (i + 1) // 2 ** (i + 1)
+		newDuration, _ := benchmarkFunc(trees[i], keyCount, threadCount)
+		fmt.Printf("Speedup over sequential: %f\n", duration.Seconds()/newDuration.Seconds())
+	}
+}
+
+func runBenchmark(benchmarkName string, benchmarkFunc func(tree_api.BPTree, int, int) (time.Duration, float64), seqTree tree_api.BPTree, globalLockTrees []tree_api.BPTree, crabTrees []tree_api.BPTree, keyCount int, maxThreadCount int) {
+	fmt.Printf("Benchmark %s\n", benchmarkName)
+	fmt.Printf("Sequential Tree %s Benchmark\n", benchmarkName)
+	benchmarkFunc(seqTree, keyCount, 1)
+	fmt.Printf("Global Lock Tree %s Benchmark\n", benchmarkName)
+	runSpeedup(globalLockTrees, keyCount, maxThreadCount, benchmarkFunc)
+	fmt.Printf("Crab Tree %s Benchmark\n", benchmarkName)
+	runSpeedup(crabTrees, keyCount, maxThreadCount, benchmarkFunc)
+}
+
+func makeTreeList(threadCount int, treeConstructor func() tree_api.BPTree) []tree_api.BPTree {
+	trees := []tree_api.BPTree{}
+	for i := threadCount; i > 0; i /= 2 {
+		trees = append(trees, treeConstructor())
+	}
+	return trees
+}
+
 func main() {
-	keyCount := 2500000
+
+	// Set up trees
+	keyCount := 1000000
 	seqTree := seq_tree.NewTree()
-	// globalLockTree := global_lock_tree.NewTree()
-	fmt.Println("Sequential Tree Insert Benchmark")
-	benchmark.RunInsertBenchmark(seqTree, keyCount, 1)
-	fmt.Println("Global Lock Tree Insert Benchmark")
-	seqGlobalTree := global_lock_tree.NewTree()
-	duration, _ := benchmark.RunInsertBenchmark(seqGlobalTree, keyCount, 1)
-	bigTrees := []*global_lock_tree.GlobalLockTree{}
-	bigTrees = append(bigTrees, seqGlobalTree.(*global_lock_tree.GlobalLockTree))
-	for threadCount := 2; threadCount <= 64; threadCount *= 2 {
-		tree := global_lock_tree.NewTree()
-		newDuration, _ := benchmark.RunInsertBenchmark(tree, keyCount, threadCount)
-		fmt.Printf("Speedup over sequential: %f\n", duration.Seconds()/newDuration.Seconds())
-		bigTrees = append(bigTrees, tree.(*global_lock_tree.GlobalLockTree))
-	}
-	fmt.Println("Sequential Tree Find Benchmark")
-	benchmark.RunFindBenchmark(seqTree, keyCount, 1)
+	maxThreadCount := 64
+	globalLockTrees := makeTreeList(maxThreadCount, global_lock_tree.NewTree)
+	crabTrees := makeTreeList(maxThreadCount, crab.NewTree)
+	// globalLockTrees := makeGlobalLockTreeList(maxThreadCount)
+	// crabTrees := makeCrabTreeList(maxThreadCount)
 
-	fmt.Println("Global Lock Tree Find Benchmark")
-	duration, _ = benchmark.RunFindBenchmark(seqGlobalTree, keyCount, 1)
-	treeIdx := 0
-	for threadCount := 2; threadCount <= 64; threadCount *= 2 {
-		newDuration, _ := benchmark.RunFindBenchmark(bigTrees[treeIdx], keyCount, threadCount)
-		fmt.Printf("Speedup over sequential: %f\n", duration.Seconds()/newDuration.Seconds())
-		treeIdx++
-	}
-	treeIdx = 0
-	fmt.Println("Sequential Tree Delete Benchmark")
-	benchmark.RunDeleteBenchmark(seqTree, keyCount, 1)
-
-	fmt.Println("Global Lock Tree Delete Benchmark")
-	duration, _ = benchmark.RunDeleteBenchmark(seqGlobalTree, keyCount, 1)
-	for threadCount := 2; threadCount <= 64; threadCount *= 2 {
-		newDuration, _ := benchmark.RunDeleteBenchmark(bigTrees[treeIdx], keyCount, threadCount)
-		fmt.Printf("Speedup over sequential: %f\n", duration.Seconds()/newDuration.Seconds())
-		treeIdx++
-	}
+	runBenchmark("Insert", benchmark.RunInsertBenchmark, seqTree, globalLockTrees, crabTrees, keyCount, maxThreadCount)
+	runBenchmark("Find", benchmark.RunFindBenchmark, seqTree, globalLockTrees, crabTrees, keyCount, maxThreadCount)
+	// runBenchmark("Delete", benchmark.RunDeleteBenchmark, seqTree, globalLockTrees, keyCount)
 
 }
